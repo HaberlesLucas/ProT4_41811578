15d6565463b0e6d1b5a40e8f0d4f80c6000f400e
Lucas <lucashaberles41q@gmail.com>
2024-08-14 20:50:47 -0300

    se añadieron validaciones correspondientes al CRUD

--- src/controllers/LibroController.js
+++ src/controllers/LibroController.js
@@ -31,13 +31,17 @@ const getOne = async (req, res) => {
 
 //crear uno
 const setLibro = async (req, res) => {
-    const { body } = req;
+    const {body } = req;
     try {
+        const existeLibro = await Libros.findOne({ where: { ISBN: body.ISBN } });
+        if (existeLibro) {
+            return res.status(400).json({ mensaje: `Ya existe un libro con el ISBN: ${body.ISBN}`  });
+        }
         const libro = await Libros.create(body);
         res.status(201).json(libro);
 
     } catch (error) {
-        return res.status(500).json({ mensaje: 'Error: ', error });
+        return res.status(500).json({ error });
     }
 }
 
@@ -51,9 +55,9 @@ const updateLibro = async (req, res) => {
         const libro = await Libros.findOne({ where: { id } });
         if (libro) {
             await libro.update({ nombre, autor, categoria, anio_publicacion, ISBN }, { where: { id } });
-            return res.status(200).json({ mensaje: 'Actualizado', libro });
+            return res.status(200).json({ mensaje: 'Libro actualizado', libro });
         } else {
-            return res.status(500).json({ mensaje: 'No encontrado' });
+            return res.status(500).json({ mensaje: 'Libro no encontrado' });
         }
     } catch (error) {
         return res.status(500).json({ mensaje: 'error', error });
@@ -64,14 +68,19 @@ const updateLibro = async (req, res) => {
 const deleteLibro = async (req, res) => {
     try {
         const { ISBN } = req.params;
-        await Libros.destroy({where: {ISBN: ISBN}});
-        return res.status(200).json({mensaje: 'Eliminado'});
-
+        //buscar 
+        const libro = await Libros.findOne({ where: { ISBN } });
+        //comprobar que exista libro buscado
+        if (libro) {
+            await libro.destroy();
+            return res.status(200).json({ mensaje: 'Libro Eliminado' });
+        } else {
+            return res.status(404).json({ mensaje: 'Libro NO Encontrado' });
+        }
     } catch (error) {
-        return res.status(500).json({mensaje: 'Error: ', error});
-        
+        return res.status(500).json({ mensaje: 'Error: ', error });
+
     }
 }
-
 
 module.exports = { getAll, getOne, setLibro, updateLibro, deleteLibro }
